bf8903c194573954c7737ff6ff13df33393b23cd
stephentetley <stephen.tetley@gmail.com>
2026-06-15 22:44:48 +0100

    Work on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// Acknowledgement - based on Andy Gill's Haskell `Text.Dot`
+
 type NodeId = string | number
 
 
@@ -33,3 +35,33 @@ let e3: ModelElement = {kind: "connector", cfrom : 1, cto:2}
 console.log(show(e1))
 console.log(show(e2))
 console.log(show(e3))
+
+
+type Dot<A> = (st: number, w: Array<ModelElement>) => {state: number, w: Array<ModelElement>, ans: A};
+
+
+function runDot<A>(fn: Dot<A>): {state: number, w: Array<ModelElement>, ans: A} { 
+    return fn(1, [])
+}
+
+
+function getDot<A>(fn: Dot<A>, ns: number, w: Array<ModelElement>): {state: number, w: Array<ModelElement>, ans: A} { 
+    return fn(ns, w)
+}
+
+function pure<A>(a: A): Dot<A>;
+function pure<A>(a: A) { 
+    return function(st: number, w: Array<ModelElement>) {
+        return {state: st, w: w, ans: a}
+    }
+}
+
+
+function fmap<A, B>(func: (a: A) => B, m: Dot<A>): Dot<B> { 
+    return function(st: number, w: Array<ModelElement>) {
+        let {state: st1, w: w1, ans: a} = getDot(m, st, w);
+        let b = func(a);
+        return {state: st1, w: w1, ans: b}
+    }
+}
+
